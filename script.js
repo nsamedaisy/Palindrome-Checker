26bc72ca7f27046759766d98f1da8507ce89a418
nsamedaisy <nsamedaisy@gmail.com>
2023-04-27 14:58:51 +0100

    fixed indentation

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-	const txtInput = document.querySelector('.inputs input');
+const txtInput = document.querySelector('.inputs input');
 
-	const checkBtn = document.querySelector('.inputs button');
+const checkBtn = document.querySelector('.inputs button');
 
-	const infoTxt = document.querySelector('.info-text');
+const infoTxt = document.querySelector('.info-text');
 
-	let filter;
+let filter;
 
-	checkBtn.addEventListener("click", () => {
-		let reverse = filter.split("").reverse().join("");
-		infoTxt.style.display = "block";
-		if(filter != reverse){
-			return infoTxt.innerHTML = `False, <span>'${txtInput.value}'</span> isn't a palindrome`;
-		}
-		infoTxt.innerHTML = `True, <span>'${txtInput.value}'</span> is a palindrome`
-	});
+checkBtn.addEventListener("click", () => {
+  let reverse = filter.split('').reverse().join('');
+  infoTxt.style.display = "block";
+  if(filter != reverse){
+    return infoTxt.innerHTML = `False, <span>'${txtInput.value}'</span> isn't a palindrome`;
+  }
+  infoTxt.innerHTML = `True, <span>'${txtInput.value}'</span> is a palindrome`
+  });
 
 
   txtInput.addEventListener("keyup", () => {
-		filter = txtInput.value.toLowerCase().replace(/[A-Z0-9]/ig, "");
-	  if(filter) {
-		return checkBtn.classList.add("active");
-	  }
-		infoTxt.style.display = "none";
-		checkBtn.classList.remove("active");
-	});
+  filter = txtInput.value.toLowerCase().replace(/[A-Z0-9]/ig, "");
+    if(filter) {
+      return checkBtn.classList.add("active");
+    }
+  infoTxt.style.display = "none";
+  checkBtn.classList.remove("active");
+});;
